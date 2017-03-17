7907c68b75190ed2e5f178aa5e546f1911722c31
Artem Makarov <artemmakaroff@mail.ru>
2017-03-17 10:26:30 +0700

    Начало работы над приложением. Время 10:26

---
+++ TaskZennex/TaskZennex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskZennex
+//
+//  Created by Artem Makarov on 17.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaskZennex/TaskZennex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaskZennex
+//
+//  Created by Artem Makarov on 17.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
