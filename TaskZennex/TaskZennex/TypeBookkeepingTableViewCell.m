791b2a3a6d3ffd185d3513bae1e0c43e2629e278
Artem Makarov <artemmakaroff@mail.ru>
2017-03-21 19:25:46 +0700

    Создаём ячейку для для контроллера выбора типа бухгалтера

---
+++ TaskZennex/TaskZennex/TypeBookkeepingTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TypeBookkeepingTableViewCell.m
+//  TaskZennex
+//
+//  Created by Artem Makarov on 21.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import "TypeBookkeepingTableViewCell.h"
+
+@implementation TypeBookkeepingTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
