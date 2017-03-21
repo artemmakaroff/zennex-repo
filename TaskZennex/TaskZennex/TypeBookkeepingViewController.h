b049d163b94a86daca13baa19f3fd803fcf74a28
Artem Makarov <artemmakaroff@mail.ru>
2017-03-21 19:22:04 +0700

    Создаём новый TableViewController для выбора типа бухгалтера

---
+++ TaskZennex/TaskZennex/TypeBookkeepingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TypeBookkeepingViewController.h
+//  TaskZennex
+//
+//  Created by Artem Makarov on 21.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TypeBookkeepingViewController : UITableViewController
+
+@end
