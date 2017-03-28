eda26a4fb0d51b0dd6824762e686aaa6c892828a
Artem Makarov <artemmakaroff@mail.ru>
2017-03-28 20:11:25 +0700

    Создание ServiceViewController (TableViewController для 3го экрана)

---
+++ TaskZennex/TaskZennex/ServiceViewController.h
@@ -0,0 +1,15 @@
+//
+//  ServiceViewController.h
+//  TaskZennex
+//
+//  Created by Artem Makarov on 28.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *serviceArray;
+
+@end
---
+++ TaskZennex/TaskZennex/ServiceViewController.m
@@ -0,0 +1,78 @@
+//
+//  ServiceViewController.m
+//  TaskZennex
+//
+//  Created by Artem Makarov on 28.03.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+#import "ServiceViewController.h"
+#import "ServiceModel.h"
+#import "ServiceDownloader.h"
+#import "ServiceViewCell.h"
+
+@interface ServiceViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation ServiceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self readDataFromJSON];
+    
+}
+//Метод возращающий нам массив который мы получили из ServiceDownloader
+- (void)readDataFromJSON
+{
+    ServiceDownloader *serviceDownloader = [ServiceDownloader parserManager];
+    self.serviceArray = [serviceDownloader parsingJSON];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.serviceArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *serviceIdentifier = @"ServiceIdentifier";
+    
+    ServiceViewCell *cell = [tableView dequeueReusableCellWithIdentifier:serviceIdentifier forIndexPath:indexPath];
+    
+    if (cell == nil) {
+        cell = [[ServiceViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:serviceIdentifier];
+    }
+    
+    ServiceModel *serviceModel = [self.serviceArray objectAtIndex:indexPath.row];
+    
+
+    
+    cell.idLabel.text = [NSString stringWithFormat:@"ID: %@", serviceModel.idString];
+    cell.descriptionLabel.text = [NSString stringWithFormat:@"%@", serviceModel.descriptionString];
+    cell.timeLabel.text = [NSString stringWithFormat:@"Время: %@", serviceModel.timeString];
+    cell.ratingLabel.text = [NSString stringWithFormat:@"Рейтинг: %@", serviceModel.ratingString];
+    
+    
+    return cell;
+}
+
+
+@end
